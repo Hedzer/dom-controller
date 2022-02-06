c57ea29c322bdc37737628d2f940666ce2d5b180
Hedzer Ferwerda <hedzer87@gmail.com>
2022-02-06 00:28:28 -0500

    Changed package to public

--- example/basic-typescript-controller.ts
+++ example/basic-typescript-controller.ts
@@ -2,7 +2,7 @@ import 'dom-controller/window';
 
 import { IController } from 'dom-controller/IController';
 
-class SomeLogic implements IController<HTMLDivElement> {
+class ToDo implements IController<HTMLDivElement> {
 	element: HTMLDivElement;
 	async attach(): Promise<void> {
 		throw new Error('Method not implemented.');
@@ -12,4 +12,4 @@ class SomeLogic implements IController<HTMLDivElement> {
 	}
 }
 
-window.DomController.registerController(SomeLogic);
+window.DomController.registerController(ToDo);
